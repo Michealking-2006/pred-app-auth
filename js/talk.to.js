3bc0d39628c18af0cf7dc6cf252636ee5b7081c3
Michealking-2006 <Michealkingg2006@gmail.com>
2026-08-06 20:27:29 +0100

    update

--- js/talk.to.js
+++
@@ -1,9 +0,0 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6a56ce39446edc1d4ae52b50/1jthhb57m';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
