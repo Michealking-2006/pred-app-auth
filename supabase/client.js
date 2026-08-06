56b34c91451759ce285be4b4947756c732c75245
Michealking-2006 <Michealkingg2006@gmail.com>
2026-08-06 22:35:38 +0100

    update client.js

--- supabase/client.js
+++ supabase/client.js
@@ -1,7 +1,7 @@
 /****** Supabase Client ******/
 
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = "https://fhsteyglvxuanyvgkkxp.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_l-EwBR_dCGpxDo_87GC1HA_1COYjj3W";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
